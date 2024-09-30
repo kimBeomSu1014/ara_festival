93dc29c2125de0d15c35637679ba157b39a96f6d
김범수 <k1014ttl@naver.com>
2024-09-30 14:00:44 +0900

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import { day1_artist,day2_artist } from './js/artist';
+
+const Home = () => (
+  <div className="content">
+    <h2>축제에 오신 것을 환영합니다!</h2>
+    <p>여기에 축제에 대한 소개 말이 들어갑니다.</p>
+    <iframe src="https://giphy.com/embed/mbar54jwxlnKs5nlOc" width="120" height="120" style={{border:"none",zIndex:"1",scrolling:"no"}} className="fire" allowFullScreen></iframe>
+    <p></p>
+  </div>
+);
+
+const Day1 = () => 
+<div className="Day_content">
+  <p>1일차 아티스트!</p>
+  <div className='artist'>
+    {day1_artist.map((artist, index)=>(
+      <div className='artist_box' key={index}>
+        <div className='artist_name'>
+          <span className='en_name'>{artist.en_name}</span>
+          <span className='ko_name'>{artist.ko_name}</span>
+        </div>
+        <div className='artist_profil'>
+          <img src={artist.profil}/>
+        </div>
+        <div className='artist_youtube'>
+        <iframe width="300" height="168" src= {artist.youtube_1}
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="300" height="168" src= {artist.youtube_2}
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+      </div>
+    ))}
+  </div>
+</div>;
+const Day2 = () => 
+  <div className="Day_content">
+  <p>2일차 아티스트!</p>
+  <div className='artist'>
+    {day2_artist.map((artist, index)=>(
+      <div className='artist_box' key={index}>
+        <div className='artist_name'>
+          <span className='en_name'>{artist.en_name}</span>
+          <span className='ko_name'>{artist.ko_name}</span>
+        </div>
+        <div className='artist_profil'>
+          <img src={artist.profil}/>
+        </div>
+        <div className='artist_youtube'>
+        <iframe width="300" height="168" src= {artist.youtube_1}
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="300" height="168" src= {artist.youtube_2}
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+      </div>
+    ))}
+  </div>
+</div>;
+const FAQ = () => <div className="content">자주하는 질문 내용</div>;
+const LostItems = () => <div className="content">분실물 내용</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="header">
+          <span>Acendio</span>
+        </header>
+        <nav className="navbar">
+          <div className="scrollable-tabs">
+            <Link to="/">메인</Link>
+            <Link to="/day1">1일차</Link>
+            <Link to="/day2">2일차</Link>
+            <Link to="/faq">자주하는 질문</Link>
+            <Link to="/lostitems">분실물</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/day1" element={<Day1 />} />
+          <Route path="/day2" element={<Day2 />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/lostitems" element={<LostItems />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/js/artist.js
@@ -0,0 +1,38 @@
+export const day1_artist = [{
+    en_name:"VIVIZ",
+    ko_name:"비비지",
+    profil: "./images/viviz_profil.webp",
+    youtube_1:"https://www.youtube.com/embed/nFHG0TeBIIs?si=1cSpr87k-dnMKBdR",
+    youtube_2:"https://www.youtube.com/embed/pCbPwpcgUbM?si=2LEPeuz8fgaAOCHd",
+  },{
+    en_name: "LIGHTSUM",
+    ko_name: "라잇썸",
+    profil: "./images/lightsum_profil.webp",
+    youtube_1:"https://www.youtube.com/embed/TKGovwXRDfU?si=oB6lMCvp4lrF5O3t",
+    youtube_2:"https://www.youtube.com/embed/y5WZ2htfiZU?si=miAZSELyz5l3_TLW",
+  },{ 
+    en_name: "TransFixion",
+    ko_name: "트랜스픽션",
+    profil: "./images/transfixion_profil.webp",
+    youtube_1:"https://www.youtube.com/embed/uP12eRXd5EE?si=CzEocmwNVRpKZenO",
+    youtube_2:"https://www.youtube.com/embed/QtUe0-QXR9Q?si=o0EEKeJqCAo-rO5K",
+  }]
+export const day2_artist = [{
+    en_name: "Woody",
+    ko_name: "우디",
+    profil: "./images/woody_profil.webp",
+    youtube_1:"https://www.youtube.com/embed/ENI8PmrW844?si=vyukKagSYvxO5dEk",
+    youtube_2:"https://www.youtube.com/embed/j4qaT26K80A?si=YF6U4tWG5XuW1KfH",
+    },{
+    en_name: "STAYC",
+    ko_name: "스테이씨",
+    profil: "./images/stayc_profil.webp",
+    youtube_1:"https://www.youtube.com/embed/s-UXxaQdUew?si=eIGcn0KPeW-C8XqT",
+    youtube_2:"https://www.youtube.com/embed/3-ptVHZZdBg?si=pH6p8xJIV0vCT-5n",
+    },{
+      en_name: "MRCH",
+      ko_name: "윤마치",
+      profil: "./images/mrch_profil.webp",
+      youtube_1:"https://www.youtube.com/embed/P8K14z7w3ec?si=GBiFh8OuIqMptOO2",
+      youtube_2:"https://www.youtube.com/embed/wFYCoiffSSI?si=35xnFf2u1I1Vh1yW",
+    }]
